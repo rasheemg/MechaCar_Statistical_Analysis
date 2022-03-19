9d0e2e6c15794a49f6a411c9c80a4bfee9fd46fe
rasheemg <93060074+rasheemg@users.noreply.github.com>
2022-03-18 22:34:22 -0400

    Challenge Deliverable #1

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,3 @@
+library(dplyr)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)) #generate summary statistics
