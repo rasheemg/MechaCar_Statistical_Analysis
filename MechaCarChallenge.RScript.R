fbfe505edadfd79d3249ba887b6819236f2aaa95
rasheemg <93060074+rasheemg@users.noreply.github.com>
2022-03-19 17:49:11 -0400

    Challenge Deliverable #3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -7,3 +7,12 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 Suspension_Coil <- read.csv("~/Rasheem Gatewood/Data Analytics Bootcamp/Module 15 Statistics and R/R_Analysis/Challenge/Suspension_Coil.csv") 
 total_summary <- Suspension_Coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep')
 lot_summary<- Suspension_Coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep')
+
+t.test(Suspension_Coil$PSI,mu=1500)
+lot1 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
